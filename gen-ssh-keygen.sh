cd33d8bb4ca70cd56b0e37fe7c7c5a4334f326ce
Sabbir Ahmed <ahmd.sabbir@gmail.com>
2023-12-05 04:56:35 +0600

    Update gen-ssh-keygen.sh modified

--- gen-ssh-keygen.sh
+++ gen-ssh-keygen.sh
@@ -11,6 +11,6 @@ ip_address="$3"
 password="$4"
 
 # Use arguments to create an SSH key pair
-ssh-keygen -t rsa -b 2048 -N "$password" -C "$remote_username@$ip_address" -f "/home/$home_username/.ssh/id_rsa"
+ssh-keygen -t rsa -b 2048 -N "$password" -C "$remote_username@$ip_address" -f "/home/$home_username/.ssh/id_rsa.pub"
 
 echo "SSH key pair created for $username on $ip_address"
