1216497bc3efc9a4d72ebb7368dffa5abfb0fec4
Sabbir Ahmed <ahmd.sabbir@gmail.com>
2023-12-05 07:05:18 +0600

    Update gen-ssh-keygen.sh

--- gen-ssh-keygen.sh
+++ gen-ssh-keygen.sh
@@ -8,10 +8,18 @@ fi
 home_username="$1"
 remote_username="$2"
 remote_ip_address="$3"
-
+alias="$4"
 # Use arguments to create an SSH key pair
 ssh-keygen -f "/home/$home_username/.ssh/id_rsa"
 
 ssh-copy-id -i /home/$home_username/.ssh/id_rsa "$remote_username@$remote_ip_address"
 
 echo "SSH key pair created for $remote_username on $remote_ip_address"
+
+if [ -n "$alias" ]; then
+    # Set an alias if provided
+    echo "alias $alias='ssh $remote_username@$remote_ip_address'" >> "/home/$home_username/.bashrc"
+    echo "Alias set: $alias"
+fi
+
+
