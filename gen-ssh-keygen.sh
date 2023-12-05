25bb654fb090015ea3798901af9bfda3c9ce570f
Sabbir Ahmed <ahmd.sabbir@gmail.com>
2023-12-05 07:19:11 +0600

    Update gen-ssh-keygen.sh

--- gen-ssh-keygen.sh
+++ gen-ssh-keygen.sh
@@ -9,7 +9,8 @@ home_username="$1"
 remote_username="$2"
 remote_ip_address="$3"
 alias="$4"
-# Use arguments to create an SSH key pair
+shell="$5"
+
 ssh-keygen -f "/home/$home_username/.ssh/id_rsa"
 
 ssh-copy-id -i /home/$home_username/.ssh/id_rsa "$remote_username@$remote_ip_address"
